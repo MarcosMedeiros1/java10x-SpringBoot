c0f31efbbdad4d142ba13bb3134445ae289b71be
MarcosMedeiros1 <contato.marcosmedeiros01@gmail.com>
2025-08-29 07:28:17 -0300

    initial commit

---
+++ src/main/java/dev/java10x/HobbitCRUD/HobbitCrudApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.HobbitCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbitCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbitCrudApplication.class, args);
+	}
+
+}
