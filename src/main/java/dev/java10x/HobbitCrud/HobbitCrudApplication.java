48f7eadaaf2d9d395d468dec29785a067497c86e
MarcosMedeiros1 <contato.marcosmedeiros01@gmail.com>
2025-09-02 07:36:57 -0300

    criando entidade no banco de dados a partir do HobbitModel

--- src/main/java/dev/java10x/HobbitCrud/HobbitCrudApplication.java
+++ src/main/java/dev/java10x/HobbitCrud/HobbitCrudApplication.java
@@ -1,4 +1,4 @@
-package dev.java10x.HobbitCRUD;
+package dev.java10x.HobbitCrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HobbitCrudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HobbitCrudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HobbitCrudApplication.class, args);
+    }
 
 }
