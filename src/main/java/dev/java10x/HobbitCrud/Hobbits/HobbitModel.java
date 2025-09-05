1dd032964e9f54ad3d055eae13973d1deac94bb0
MarcosMedeiros1 <contato.marcosmedeiros01@gmail.com>
2025-09-05 06:36:52 -0300

    feat: adding Lombok to classes

--- src/main/java/dev/java10x/HobbitCrud/Hobbits/HobbitModel.java
+++ src/main/java/dev/java10x/HobbitCrud/Hobbits/HobbitModel.java
@@ -2,11 +2,17 @@ package dev.java10x.HobbitCrud.Hobbits;
 
 import dev.java10x.HobbitCrud.Missoes.MissaoModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_hobbits")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class HobbitModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,37 +23,4 @@ public class HobbitModel {
     @ManyToOne
     @JoinColumn(name = "missao_id")
     private MissaoModel missoes;
-
-    public HobbitModel() {
-    }
-
-    public HobbitModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
