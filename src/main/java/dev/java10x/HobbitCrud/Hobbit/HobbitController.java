8f173840ca83b95dbb608e8a2ea65bfae29fbcf8
MarcosMedeiros1 <contato.marcosmedeiros01@gmail.com>
2025-09-19 06:29:23 -0300

    feat: update HobbitController with mocked data

--- src/main/java/dev/java10x/HobbitCrud/Hobbit/HobbitController.java
+++ src/main/java/dev/java10x/HobbitCrud/Hobbit/HobbitController.java
@@ -1,8 +1,6 @@
 package dev.java10x.HobbitCrud.Hobbit;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,35 @@ public class HobbitController {
     @GetMapping("/helloWorld")
     public String helloWorld() {
         return "Hello World!";
+    }
+
+    // Add Hobbit
+    @PostMapping("/create")
+    public String CreateHobbit() {
+        return "Created!";
+    }
+
+    // Get Hobbit
+    @GetMapping("all")
+    public String getAllHobbits() {
+        return "Showing all hobbits";
+    }
+
+    // Get Hobbit by ID
+    @GetMapping("/hobbitId")
+    public String getHobbitById() {
+        return "Showing one specific hobbit";
+    }
+
+    // Update Hobbit data
+    @PutMapping("/updateId")
+    public String updateHobbitById() {
+        return "Updated!";
+    }
+
+    // Delete Hobbit
+    @DeleteMapping("/deleteId")
+    public String deleteHobbitById() {
+        return "Deleted!";
     }
 }
