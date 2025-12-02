b8a3ba15ca91fb23ab0cadef03169e532e4860b3
MarcosMedeiros1 <contato.marcosmedeiros01@gmail.com>
2025-12-02 07:04:07 -0300

    refactor: update and delete missionController

--- src/main/java/dev/java10x/HobbitCrud/Mission/MissionController.java
+++ src/main/java/dev/java10x/HobbitCrud/Mission/MissionController.java
@@ -1,6 +1,7 @@
 package dev.java10x.HobbitCrud.Mission;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
@@ -37,13 +38,32 @@ public class MissionController {
                 .body("Mission created: " + newMission.getName() + " - ID " + newMission.getId());
     }
 
-    @PutMapping("/update")
-    public String updateMission() {
-        return "Mission updated!";
+    @PutMapping("/update/{id}")
+    @Operation(summary = "Update Misssion by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Mission updated"),
+            @ApiResponse(responseCode = "404", description = "Mission not found")
+    })
+    public ResponseEntity<?> updateMission(
+            @Parameter(description = "Send ID on path")
+            @PathVariable Long id,
+            @RequestBody MissionDTO mission)
+    {
+        if (missionService.getMissionById(id) == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Mission with ID " + id + " not found.");
+        }
+        MissionDTO missionDTO = missionService.updateMission(id, mission);
+        return ResponseEntity.ok(missionDTO);
     }
 
-    @DeleteMapping("")
-    public String deleteMission() {
-        return "Mission deleted!";
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteMission(@PathVariable Long id) {
+        if (missionService.getMissionById(id) == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Mission with ID " + id + " not found.");
+        }
+        missionService.deleteMissionById(id);
+        return ResponseEntity.ok("Mission with ID " + id + " deleted.");
     }
 }
